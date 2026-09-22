0c2829094ce7b21cb73729cbc9cdbf4826ae72b8
Felix Lie <felixlie@Felixs-MacBook-Air.local>
2026-09-22 13:37:31 +0700

    connect to DB

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const knex = require('knex');
+
+const postgres = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'felixlie',
+    password: '',
+    database: 'smart-brain',
+  },
+});
+
+console.log(postgres.select("*").from('users'));
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
