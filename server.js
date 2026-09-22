f5650da599df0b97dd46387c274ad928647707e7
Felix Lie <felixlie@Felixs-MacBook-Air.local>
2026-09-22 16:03:31 +0700

    sign in functionality added

--- server.js
+++ server.js
@@ -22,68 +22,76 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '1234',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-
-    ]
-}
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-app.post('/signin', (req,res) => {
-    if (req.body.email === database.users[0].email 
-        && req.body.password === database.users[0].password){
-            res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in')
-    }
-})
+app.post('/signin', (req, res) => {
+    db.select('email', 'hash')
+        .from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            bcrypt.compare(req.body.password, data[0].hash)
+                .then(isValid => {
+                    if (isValid) {
+                        return db.select('*')
+                            .from('users')
+                            .where('email', '=', req.body.email)
+                            .then(user => {
+                                res.json(user[0]);
+                            })
+                            .catch(err =>
+                                res.status(400).json('Unable to get user')
+                            );
+                    } else {
+                        res.status(400).json('Wrong credentials');
+                    }
+                })
+                .catch(err => {
+                    res.status(400).json('Wrong credentials');
+                });
+        })
+        .catch(err => {
+            res.status(400).json('Wrong credentials');
+        });
+});
 
-app.post('/register', (req,res) => {
+app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    // bcrypt.genSalt(10, function(err, salt) {
-    //     bcrypt.hash(password, salt, function(err, hash) {
-    //         console.log(hash);
-    //     });
-    // });
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
+    const hash = bcrypt.hashSync(password, 10);
+    db.transaction(trx => {
+        return trx('login')
+            .insert({
+                hash: hash,
+                email: email
+            })
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0].email,
+                        name: name,
+                        joined: new Date()
+                    });
+            });
     })
     .then(user => {
         res.json(user[0]);
     })
-    .catch(err => res.status(400).json('unable to register'))
-})
+    .catch(err => {
+        console.log(err);
+        res.status(400).json('Unable to register');
+    });
+});
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     db.select('*').from('users').where({
         id: id
     }).then(user => {
-        console.log(user);
         if (user.length) {
             return res.json(user[0]);
         } else {
@@ -108,24 +116,3 @@ app.put('/image', (req,res) => {
 app.listen(3000, () => {
     console.log('app is running on port 3000')
 })
-
-
-/* 
-
-/ --> res = this is working
-/signin --> POST = success or fail
-/register --> POST = user
-/profile/:userId -> GET = user
-/image --> PUT --> user 
-
-*/
-
-
-// .then(entries => {
-//     // If you are using knex.js version 1.0.0 or higher this now 
-//     // returns an array of objects. Therefore, the code goes from:
-//     // entries[0] --> this used to return the entries
-//     // TO
-//     // entries[0].entries --> this now returns the entries
-//     res.json(entries[0].entries);
-//   })
