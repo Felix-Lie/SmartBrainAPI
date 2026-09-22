a52dbf0615ef973a88dd060535591a9a9e6719ea
Felix Lie <felixlie@Felixs-MacBook-Air.local>
2026-09-22 14:25:55 +0700

    updating entries

--- server.js
+++ server.js
@@ -96,17 +96,13 @@ app.get('/profile/:id', (req, res) => {
 app.put('/image', (req,res) => {
     console.log('Image Body: ', req.body);
     const { id } = req.body;
-    let found = false
-    database.users.forEach(user => {
-        if (user.id === id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(404).json('not such user');
-    }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('Unable to get entries'))
 })
 
 app.listen(3000, () => {
@@ -123,3 +119,13 @@ app.listen(3000, () => {
 /image --> PUT --> user 
 
 */
+
+
+// .then(entries => {
+//     // If you are using knex.js version 1.0.0 or higher this now 
+//     // returns an array of objects. Therefore, the code goes from:
+//     // entries[0] --> this used to return the entries
+//     // TO
+//     // entries[0].entries --> this now returns the entries
+//     res.json(entries[0].entries);
+//   })
