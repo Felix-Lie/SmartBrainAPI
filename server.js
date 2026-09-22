3dd1f4b4d6a49cf8fe4e9d9ae6f86eae3e7bf721
Felix Lie <felixlie@Felixs-MacBook-Air.local>
2026-09-22 14:07:31 +0700

    Getting User Profiles

--- server.js
+++ server.js
@@ -80,16 +80,17 @@ app.post('/register', (req,res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false
-    database.users.forEach(user => {
-        if (user.id === id){
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({
+        id: id
+    }).then(user => {
+        console.log(user);
+        if (user.length) {
+            return res.json(user[0]);
+        } else {
+            res.status(400).json('Not found')
         }
-    });
-    if (!found) {
-        res.status(404).json('no such user');
-    }
+    })
+    .catch(err => res.status(400).json('Error Getting User'))
 })
 
 app.put('/image', (req,res) => {
